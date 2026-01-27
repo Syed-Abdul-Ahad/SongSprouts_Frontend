a47d7711f921314c735b62e8a4e6054f8cdb1d02
ahad <engrabdulahad924@gmail.com>
2026-01-27 22:12:38 +0500

    configured axios, api, context and tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
   theme: {
     extend: {},
   },
-  plugins: [
-     tailwindcss(),
-  ],
+  plugins: [],
 }
