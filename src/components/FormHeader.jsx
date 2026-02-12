ed705f016f6fd77b25887efb70f967cdad129647
Arham097 <arhamhasan70@gmail.com>
2026-02-12 23:28:01 +0500

    [FIX]: Responsiveness of AddOns and Review and Pay PAge

--- src/components/FormHeader.jsx
+++ src/components/FormHeader.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const FormHeader = ({title, description}) => {
   return (
     <div className="text-center space-y-2 mb-8">
-          <h1 className="text-3xl font-bold text-gray-900">{title}</h1>
-          <p className="text-gray-600 max-w-2xl mx-auto">
+          <h1 className="text-2xl md:text-3xl font-bold text-gray-900">{title}</h1>
+          <p className="text-gray-600 max-w-2xl mx-auto max-sm:text-sm">
             {description}
           </p>
         </div>
